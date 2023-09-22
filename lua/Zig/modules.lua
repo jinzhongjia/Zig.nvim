4eaf6219a8a3488d527d601eaa763711d14316f8
jinzhongjia <mail@nvimer.org>
2023-09-22 12:50:23 +0800

    format basic

--- lua/Zig/modules.lua
+++ lua/Zig/modules.lua
@@ -1,3 +1,7 @@
 return {
     autocmd = require("Zig.autocmd"),
+    build = require("Zig.build"),
+    test = require("Zig.test"),
+    format = require("Zig.format"),
+    dependence = require("Zig.dependence"),
 }
