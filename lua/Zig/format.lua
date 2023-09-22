40c1d31a21825c20f934cc91d3024dedb011a446
jinzhongjia <mail@nvimer.org>
2023-09-22 12:50:23 +0800

    format basic

---
+++ lua/Zig/format.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+local function format_file() end
+
+local function format_buffer() end
+
+local function format_range() end
+
+M.init = function() end
+
+M.run = function() end
+
+return M
