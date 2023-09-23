882dee7ccfcd7143d50695d1a644364772a55de6
jinzhongjia <mail@nvimer.org>
2023-09-23 20:54:36 +0800

    support file format
    todo: Pipe status monitoring

--- lua/Zig/lib/async.lua
+++ lua/Zig/lib/async.lua
@@ -1,33 +1,28 @@
 local uv = vim.uv
-local lib_debug = require("Zig.lib.debug")
 
 local M = {}
 
 --- @param path string
 --- @param options uv.spawn.options
---- @param on_exit  uv.spawn.on_exit
---- @param on_out uv.read_start.callback
---- @param on_err uv.read_start.callback
+--- @param on_exit uv.spawn.on_exit?
+--- @param on_out uv.read_start.callback?
+--- @param on_err uv.read_start.callback?
 M.spawn = function(path, options, on_exit, on_out, on_err)
-    local stdin = assert(uv.new_pipe())
-    local stdout = assert(uv.new_pipe())
-    local stderr = assert(uv.new_pipe())
-
-    ---@diagnostic disable-next-line: assign-type-mismatch
-    options.stdio = { stdin, stdout, stderr }
-
-    -- local async = uv.new_async(function()
     local handle, pid = uv.spawn(path, options, on_exit)
-    -- lib_debug.debug(uv.spawn(path, options, on_exit))
-    -- lib_debug.debug("running")
-    -- read stdout
-    uv.read_start(stdout, on_out)
-    -- read stderr
-    uv.read_start(stderr, on_err)
-    -- end)
 
-    -- return stdin, async
-    return stdin, { handle = handle, pid = pid }
+    if options.stdio and options.stdio[2] and on_out then
+        -- read stdout
+        ---@diagnostic disable-next-line: param-type-mismatch
+        uv.read_start(options.stdio[2], on_out)
+    end
+
+    if options.stdio and options.stdio[3] and on_err then
+        -- read stderr
+        ---@diagnostic disable-next-line: param-type-mismatch
+        uv.read_start(options.stdio[3], on_err)
+    end
+
+    return { handle = handle, pid = pid }
 end
 
 return M
